02e44525129d4654d5fe9174057d184f90df4ce9
Keshav mehta <keshav456mehta@gmail.com>
2026-08-16 17:18:18 +0530

    Time: 0 ms (100%), Space: 42.1 MB (52.92%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,19 +1,20 @@
-class Solution 
-{
-    public int f(int n)
-    {
-        if(n == 0)
-        {
-            return 0;
+class Solution {
+    public int fib(int n) {
+        if (n <= 1) {
+            return n;
         }
-        else if(n == 1)
-        {
-            return 1;
+
+        int a = 0;
+        int b = 1;
+        int count = 2;
+
+        while(count <= n){
+            int temp = b;
+            b = b + a;
+            a = temp;;
+            count++;
         }
-        return f(n-2) + f(n-1);
-    }
-    public int fib(int n) 
-    {
-        return f(n);
+         
+         return b;
     }
 }
